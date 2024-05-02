21417a3580eafe5902ee078cc3724f00ab624389
Sevak Harutyunyan <sevak.harutyunyan@portmind.com>
2024-05-02 12:25:46 +0400

    bump: version 1.0.1

--- inferio_x/__init__.py
+++ inferio_x/__init__.py
@@ -3,5 +3,5 @@ from inferio_x._file import FileUpload
 from inferio_x._method import Method
 from inferio_x._service import Service
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __all__ = ["Endpoint", "Service", "Method", "FileUpload"]
